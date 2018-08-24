bc8dd411a291eb0e6758c7a6d7b4d15bd2e7f869
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-08-23 21:50:40 -0500

    Create table routes

---
+++ routes/tables.js
@@ -0,0 +1,133 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Table = require('../models/table');
+
+const router = express.Router();
+
+// Protect endpoints using JWT Strategy
+router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
+
+/* ========== GET/READ ALL ITEMS ========== */
+router.get('/', (req, res, next) => {
+  Table.find()
+    .sort('number')
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== GET/READ A SINGLE ITEM ========== */
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  /***** Never trust users - validate input *****/
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  Table.findOne({ _id: id })
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== POST/CREATE AN ITEM ========== */
+router.post('/', (req, res, next) => {
+  const { number } = req.body;
+
+  const newTable = { number };
+
+  /***** Never trust users - validate input *****/
+  if (!number) {
+    const err = new Error('Missing `number` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  Table.create(newTable)
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('Table number already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+/* ========== PUT/UPDATE A SINGLE ITEM ========== */
+router.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { number } = req.body;
+
+  /***** Never trust users - validate input *****/
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  if (!number) {
+    const err = new Error('Missing `number` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const updateTable = { number };
+
+  Table.findByIdAndUpdate(id, updateTable, { new: true })
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      if (err.code === 11000) {
+        err = new Error('Table number already exists');
+        err.status = 400;
+      }
+      next(err);
+    });
+});
+
+/* ========== DELETE/REMOVE A SINGLE ITEM ========== */
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  /***** Never trust users - validate input *****/
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  Table.findOneAndRemove({ _id: id })
+    .then(() => {
+      res.status(204).end();
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+module.exports = router;
