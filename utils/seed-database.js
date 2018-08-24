75edd2e96cea5cd25713e7f3c1ef2a90534708b0
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-08-24 00:31:29 -0500

    Add check to seed utility

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -3,10 +3,12 @@
 const mongoose = require('mongoose');
 
 const { DATABASE_URL } = require('../config');
+const Check = require('../models/check');
 const Table = require('../models/table');
 const Item = require('../models/item');
 const User = require('../models/user');
 
+const seedChecks = require('../db/seed/checks');
 const seedTables = require('../db/seed/tables');
 const seedItems = require('../db/seed/items');
 const seedUsers = require('../db/seed/users');
@@ -20,6 +22,8 @@ mongoose.connect(DATABASE_URL, { useNewUrlParser: true })
     seedUsers.forEach((user, i) => user.password = digests[i]);
 
     return Promise.all([
+      Check.insertMany(seedChecks),
+
       Table.insertMany(seedTables),
 
       Item.insertMany(seedItems),
