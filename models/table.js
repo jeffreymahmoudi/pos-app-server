a00a619bc5f00a15f92a9a863b3841fcf77e5ed1
Jeffrey Mahmoudi <jeffreymahmoudi@gmail.com>
2018-08-23 21:47:43 -0500

    Create table model

---
+++ models/table.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tableSchema = Schema({
+  number: { type: Number, required: true, unique: true }
+});
+
+tableSchema.set('toObject', {
+  transform: function(doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+module.exports = mongoose.model('Table', tableSchema);
